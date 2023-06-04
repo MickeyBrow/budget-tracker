65e7fee25cd1314ec6ce704dc0aa8825ba67b22b
Mickey Brown <mickisbrown10@gmail.com>
2023-06-04 17:20:17 -0600

    adding cards to potentially use to display info to the user

--- front-end/src/app/Dashboard/page.js
+++ front-end/src/app/Dashboard/page.js
@@ -1,5 +1,27 @@
+import './page.css'
+
 export default function Dashboard() {
   return (
-    <div>This will be the dashboard page.</div>
+    <>
+      <div>
+        <div class="card">
+          <div class="head">
+            <h4><b>$400</b></h4>
+          </div>
+          <div class="container">
+            <h4><b>Income</b></h4>
+          </div>
+        </div>
+        <div class="card">
+          <div class="head">
+            <h4><b>$200</b></h4>
+          </div>
+          <div class="container">
+            <h4><b>Expenses</b></h4>
+          </div>
+        </div>
+      </div>
+      <div>All divs below here will be for the month cards</div>
+    </>
   )
 }
