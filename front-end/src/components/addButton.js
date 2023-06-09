9653a7172d39f35e1329dc37f6d60b0296e52453
Mickey Brown <mickisbrown10@gmail.com>
2023-06-09 15:11:24 -0600

    add data modal is basically done now

--- front-end/src/components/addButton.js
+++ front-end/src/components/addButton.js
@@ -6,10 +6,6 @@ import { useState } from 'react';
 
 const AddButton = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [firstPage, setFirstPage] = useState(true);
-  const [secondPage, setSecondPage] = useState(false);
-  const [amount, setAmount] = useState(0);
-  const [category, setCategory] = useState('')
 
   const openModal = () => {
     setIsOpen(true);
@@ -19,36 +15,10 @@ const AddButton = () => {
     setIsOpen(false);
   };
 
-  const onSubmitClick = () => {
-    console.log("yay");
-  };
-
   return (
     <div className="topnav">
       <a onClick={openModal}>Add Data</a>
-      <AddDataModal isOpen={isOpen} onClose={closeModal} onSumbit={onSubmitClick}>
-        <h2>What Data Would you like to add?</h2>
-        <hr/>
-        {firstPage && 
-          <select>
-            <option value="Income">Income</option>
-            <option value="Expense">Expense</option>
-            <option value="Bill">Bill</option>
-          </select>
-        }
-        {secondPage && 
-          <h4>Second Page</h4>
-        }
-        {/* <label>
-          Amount: <input defaultValue="$" onChange={(e) => {setAmount(e.target.value)}}></input>
-        </label>
-        <label style={{paddingLeft: "5px"}}>
-          Category: <select onChange={(e) => {setCategory(e.target.value)}}>
-            <option>1</option>
-          </select>
-        </label> */}
-      </AddDataModal>
-
+      <AddDataModal isOpen={isOpen} onClose={closeModal}/>
     </div>
   );
 };
--- front-end/src/components/addDataModal.js
+++ front-end/src/components/addDataModal.js
@@ -1,17 +1,116 @@
 import './../componentStyles/addDataModal.css'
+import { useState } from 'react';
 
-const AddDataModal = ({ isOpen, onClose, children }) => {
+const AddDataModal = ({ isOpen, onClose }) => {
   if (!isOpen) {
     return null;
+  }
+
+  const [firstPage, setFirstPage] = useState(true);
+  const [firstPageChoice, setFirstPageChoice] = useState();
+
+  const closeModal = () => {
+    setFirstPage(true);
+    onClose();
+  };
+
+  const secondPageChoiceSwitch = (choice) => {
+    switch(choice) {
+      case "Income":
+        return (
+          <>
+            <option value="">---</option>
+            <option>Paycheck</option>
+          </>
+        );
+      case "Expense":
+        return (
+          <>
+            <option value="">---</option>
+            <option>Groceries</option>
+            <option>Entertainment</option>
+            <option>Bills</option>
+            <option>Eating Out</option>
+          </>
+        );
+      case "Bill":
+        return (
+          <>
+            <option value="">---</option>
+            <option>Mortgage</option>
+            <option>Water & Gas</option>
+            <option>Electric</option>
+            <option>HOA</option>
+            <option>Internet</option>
+            <option>Phone</option>
+            <option>Cable</option>
+          </>
+        );
+    }
+  };
+
+  const firstPageContinue = () => {
+    const choice = document.getElementById('choice');
+
+    if (!choice.value) return;
+    setFirstPageChoice(choice.value);
+    setFirstPage(false);
+  };
+
+  const onSubmit = () => {
+    const amount = document.getElementById('amount');
+    const category = document.getElementById('category');
+
+    // Check to make sure amount has a $ at the beginning and only integers after
+    console.log(amount.value);
+    console.log(category.value);
+
+    // Pass the values to firebase to get in the DB
+    closeModal();
   }
 
   return (
     <div className="modalOverlay">
       <div className="modal">
-        <button className="closeButton" onClick={onClose}>
-          X
-        </button>
-        <div className="modalContent">{children}</div>
+        <div>
+          <button className="closeButton" onClick={closeModal}>
+            X
+          </button>
+          <h4>Add Data Modal</h4>
+          <hr/>
+        </div>
+        <div className="modalContent">
+          {firstPage ?
+          <>
+            <label>
+              What type of input is this? <select id="choice">
+                <option value="">---</option>
+                <option value="Income">Income</option>
+                <option value="Expense">Expense</option>
+                <option value="Bill">Bill</option>
+              </select>
+            </label>
+          </>
+          : 
+          <>
+            <h4>{firstPageChoice}</h4>
+            <label>
+              Amount: <input id="amount"/>
+            </label>
+            <label style={{marginLeft: "10px"}}>
+              Category: <select id="category">
+                {secondPageChoiceSwitch(firstPageChoice)}
+              </select>
+            </label>
+          </>}
+        </div>
+        <div className="modalTail">
+        {firstPage ?
+          <button className="submitButton" onClick={firstPageContinue}>Continue</button>
+          : 
+          <button className="submitButton" onClick={onSubmit}>Submit</button>
+        }
+        </div>
       </div>
     </div>
   );
