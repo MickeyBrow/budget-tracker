fad6895d9fcf59676592e50d8a306e726832bc14
Mickey Brown <mickisbrown10@gmail.com>
2023-06-12 19:01:08 -0600

    adding endpoint to flask for adding data to the db

--- back-end/src/server.py
+++ back-end/src/server.py
@@ -1,8 +1,41 @@
-from flask import Flask
+import firebase_admin
+import uuid
+from flask import Flask, request
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("../secrets/serviceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def server():
+  return 'Hello, World!'
+
+@app.route('/data', methods=['POST'])
+def addData():
+  # Need to add the data that is being passed here. 
+  uid, table, amount, category, month = request.args.get('uid'), request.args.get('table'), request.args.get('amount'), request.args.get('category'), request.args.get('month')
+  
+  temp_ref = db.collection(uid).document('Dashboard').collection(month).document(table)
+
+  current = temp_ref.get()
+  if current.exists:
+    current = current.to_dict()
+  else:
+    current = {}
+
+  id = str(uuid.uuid4())
+  current["amount/category/" + id] = str(amount) + "/" + str(category)
+  temp_ref.set(current)
+
+  return {}
+
+@app.route('/data', methods=['GET'])
+def getData():
+  # This will be where we get the data for a specific month. Need a param here in the url so I can see what months data needs to be grabbed
   return 'Hello, World!'
