e8b960300fd1fb9030db18082ad6a16aee88f0dd
Mickey Brown <mickisbrown10@gmail.com>
2023-06-18 19:54:16 -0600

    signing out works

--- front-end/src/components/sidePanel.js
+++ front-end/src/components/sidePanel.js
@@ -1,10 +1,23 @@
+'use client'
 import './../componentStyles/SidePanel.css'
+import firebase_app from '@/config';
+import { useRouter } from 'next/navigation'
+import { signOut, getAuth } from "firebase/auth";
+
 const SidePanel = () => {
+  let router = useRouter();
+
+  const handleSignOut = () => {
+    const auth = getAuth(firebase_app)
+    signOut(auth).then(() => router.push('/'))
+  }
+
   return (
     <>
       <div class="sidenav">
         <a href="/Dashboard">Dashboard</a>
         <a href="/Investments">Investments</a>
+        <a onClick={handleSignOut}>Sign Out</a>
       </div>
     </>
   );
