7655a56dfc4122acd5d53fda43f1420e7727e8ae
Mickey Brown <mickisbrown10@gmail.com>
2023-06-22 15:46:06 -0600

    adding helper functions to server file instead of importing

--- back-end/env/src/server.py
+++ back-end/env/src/server.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 import os
 
-from helpers import *
 from flask import Flask, request
 from flask_cors import CORS
 from firebase_admin import credentials, firestore
@@ -29,7 +28,55 @@ cred = credentials.Certificate(my_creds)
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+##########################################################################
+# helper funtions 
 
+def formatMonthData(data):
+  def helper(pair):
+    return pair.split('/')
+
+  for table in data:
+    del data[table]['total']
+    data[table] = list(map(helper, data[table].values()))
+  
+  return data
+
+def totalPerExpenseCategory(data):
+  response = {
+    'Groceries': 0,
+    'Entertainment': 0,
+    'Eating Out': 0,
+  }
+
+  for pair in data['Expense']:
+    response[pair[1]] += float(pair[0][1:]) 
+  
+  return response
+
+def formatDataResponse(data, ExpenseTotals):
+  response = {
+    'Income_amount': [],
+    'Income_category': [],
+    'Expense_amount': [],
+    'Expense_category': [],
+    'Bill_amount': [],
+    'Bill_category': [],
+    'Expense_totals': ExpenseTotals,
+  }
+
+  for table in data:
+    for pair in data[table]:
+      response[f'{table}_amount'].append(pair[0])
+      response[f'{table}_category'].append(pair[1])
+  
+  return response
+
+def formatSummaryData(data):
+  for table in data:
+    data[table] = data[table]['total']
+  
+  return data
+##########################################################################
 
 app = Flask(__name__)
 CORS(app)
