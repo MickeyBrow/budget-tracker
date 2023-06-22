9a315f9ec88f781d37604b3b304eed657503bf87
Mickey Brown <mickisbrown10@gmail.com>
2023-06-22 11:40:40 -0600

    added env variables to flask for the creds

--- back-end/src/server.py
+++ back-end/src/server.py
@@ -1,12 +1,32 @@
 import firebase_admin
 import uuid
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 
 from helpers import *
 from flask import Flask, request
 from flask_cors import CORS
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("../secrets/serviceAccountKey.json")
+my_creds = {
+  "type": os.environ.get("FIREBASE_ACCOUNT_TYPE"),
+  "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+  "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+  "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace(r'\n', '\n'),
+  "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+  "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+  "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+  "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+  "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(my_creds)
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
